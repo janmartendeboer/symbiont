267feb67a79b318666524ac87f56065806f986ed
Jan-Marten de Boer <janmarten.deboer@mediact.nl>
2019-10-20 16:47:38 +0200

    Replace MediaCT copyright notice
    The MediaCT copyright notice was used by accident. The notice has been
    replaced by one specific to the current project.

--- src/Language/Tokenizer/Strategy/EqualsAssignmentOperatorStrategy.php
+++ src/Language/Tokenizer/Strategy/EqualsAssignmentOperatorStrategy.php
@@ -1,7 +1,11 @@
 <?php
 /**
- * Copyright MediaCT. All rights reserved.
- * https://www.mediact.nl
+ * This file is part of the Symbiont package.
+ *
+ * (c) Jan-Marten de Boer <symbiont@janmarten.name>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Symbiont\Language\Tokenizer\Strategy;
