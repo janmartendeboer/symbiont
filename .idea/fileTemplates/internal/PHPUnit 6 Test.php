f0891104760e552fdc432f1c5e5ac3c766ed8782
Jan-Marten de Boer <github@johmanx.com>
2020-10-30 13:11:30 +0100

    Store IDE configuration in project repository

---
+++ .idea/fileTemplates/internal/PHPUnit 6 Test.php
@@ -0,0 +1,20 @@
+<?php
+#parse("PHP File Header.php")
+
+#if (${NAMESPACE})
+namespace ${NAMESPACE};
+#end
+
+#if (${TESTED_NAME} && ${NAMESPACE} && !${TESTED_NAMESPACE})
+use ${TESTED_NAME};
+#elseif (${TESTED_NAME} && ${TESTED_NAMESPACE} && ${NAMESPACE} != ${TESTED_NAMESPACE})
+use ${TESTED_NAMESPACE}\\${TESTED_NAME};
+#end
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \\${TESTED_NAMESPACE}\\${TESTED_NAME}
+ */
+class ${NAME} extends TestCase {
+
+}
