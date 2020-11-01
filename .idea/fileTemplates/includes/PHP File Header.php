36ad07ffe2f0230a483fa7cc866e9f099ae82c7b
Jan-Marten de Boer <github@johmanx.com>
2020-11-01 14:16:34 +0100

    Enable strict types

--- .idea/fileTemplates/includes/PHP File Header.php
+++ .idea/fileTemplates/includes/PHP File Header.php
@@ -1,3 +1,4 @@
+
 /**
  * This file is part of the Symbiont package.
  *
@@ -6,3 +7,5 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+declare(strict_types=1);
